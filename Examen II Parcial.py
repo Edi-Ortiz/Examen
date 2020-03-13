88e971577064514237256bdf691b42c4ec0dbb77
Edi Ortiz <ediortiz222@gmail.com>
2020-03-13 16:30:14 -0600

    Examen

---
+++ Examen II Parcial.py
@@ -0,0 +1,20 @@
+# -*- coding utf8 -#-
+
+class Estacionamientovehículos(self):
+    
+    def __init__(self):
+        self.numeroPlaca = 0
+        self.marca
+        self.modelo
+        self.tipoVehiculo
+        self.horaIngreso
+        self.estado = True        
+        
+        
+        
+"""1. Número de placa del vehículo
+2. Marca
+3. Modelo
+4. Tipo de vehículo (automóvil o motocicleta)
+5. Hora de ingreso.
+6. Estado (true por defecto)."""
